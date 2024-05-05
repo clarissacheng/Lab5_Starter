0acc91c43175c801719f423977a92609af1c48d0
Dhaivat <dhaivatpachchigar@gmail.com>
2024-05-05 09:45:06 -0700

    done with the rest of the test cases

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,46 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+//testing strong password
+test('expect 123456789 to be false', ()=>{
+  expect(isStrongPassword('123456789')).toBe(false);
+});
+
+test('expect "abcdedf12738294829482" to be false', ()=>{
+  expect(isStrongPassword("abcdedf12738294829482")).toBe(false);
+});
+
+
+// testing isDate
+test('expect 12/12/2021 to be true', () => {
+  expect(isDate("12/12/2021")).toBe(true);
+});
+test('expect 2/2/2021 to be true', () => {
+  expect(isDate("2/2/2021")).toBe(true);
+});
+test('expect 2/2/201 to be false', () => {
+  expect(isDate("2/2/201")).toBe(false);
+});
+test('expect 2/222/2022 to be false', () => {
+  expect(isDate("2/222/2022")).toBe(false);
+});
+
+
+// testing hexcodes
+test('expect #1F7 to be true', () => {
+  expect(isHexColor("#1F7")).toBe(true);
+});
+test('expect #AAABBB to be true', () => {
+  expect(isHexColor("#AAABBB")).toBe(true);
+});
+test('expect #AABBCG to be true', () => {
+  expect(isHexColor("#AABBCG")).toBe(false);
+});
+test('expect AAFFFFF to be true', () => {
+  expect(isHexColor("AAFFFFF")).toBe(false);
+});
+
+
+
+
